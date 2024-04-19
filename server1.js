4ac859bb200e13dc9e39b5e45df26a324854496e
Lucas Gelenski <gelenskilucas@gmail.com>
2024-04-19 08:24:22 -0300

    criando server

--- server1.js
+++ server1.js
@@ -1,1 +1,8 @@
 // 19/04 - Criação do primeiro server web com node.js
+const http = require("http");
+const server = http.createServer((req, res) => {
+  console.log("Efetuando pedido!\n");
+});
+server.listen(3000, "localhost", () => {
+  console.log("Servidor no ar!");
+});
