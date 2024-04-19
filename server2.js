ce51b122624ca6e9fea8fed28f1385d2f2da717e
Lucas Gelenski <gelenskilucas@gmail.com>
2024-04-19 12:05:45 -0300

    identando e corrigindo o código

--- server2.js
+++ server2.js
@@ -1,16 +1,23 @@
 // Criação de um server que responde com uma página html
-const http = require("http");
-const fs = require("fs");
+const http = require('http')
+const fs = require('fs')
 
-let port = 3000;
-let host = "localhost";
+let port = 3000
+let host = 'localhost'
 
 const server = http.createServer((req, res) => {
-  res.setHeader("content-type", "text/html");
-  fs.readFile("page1.html", (erro) => {});
-  res.end();
-});
+    res.setHeader('content-type', 'text/html')
+    fs.readFile('page1.html', (erro, data) => {
+        if (erro) {
+            res.write('Deu errado, sinto muito!')
+            res.end()
+        } else {
+            res.write(data)
+            res.end()
+        }
+    })
+})
 
 server.listen(port, host, () => {
-  console.log("Servidor inicializado");
-});
+    console.log('Servidor inicializado')
+})
