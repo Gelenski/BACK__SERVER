9ce6abbc4e373347c907e57365b55f6753d53fe7
Lucas Gelenski <gelenskilucas@gmail.com>
2024-04-22 08:49:14 -0300

    criando a mudança de páginas

--- server3.js
+++ server3.js
@@ -1,14 +1,52 @@
 // * Abrir pasta que contém Html e executar através de estrutura de decisão alguma página selecionada (sistema de rotas simples).
 const http = require("http");
 const fs = require("fs");
+const { error } = require("console");
 
 const port = 3000;
 const host = "localhost";
 
 const server = http.createServer((req, res) => {
+  // Header
   res.setHeader("content-type", "text/html");
+
+  let html_page = "";
+  switch (req.url) {
+    case "/":
+      html_page = "home.html";
+      res.statusCode = 200;
+      break;
+    case "/home":
+      html_page = "home.html";
+      res.statusCode = 200;
+      break;
+    case "/servicos":
+      html_page = "servicos.html";
+      res.statusCode = 200;
+      break;
+    case "/sobre":
+      html_page = "sobre.html";
+      res.statusCode = 200;
+      break;
+    default:
+      html_page = "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  // Abertura das páginas Html.
+  fs.readFile("./html/" + html_page, (erro, data) => {
+    if (erro) {
+      console.log(erro);
+      res.end;
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
+
   res.end();
 });
 server.listen(port, host, () => {
-  console.log("Servidor em execução");
+  console.log("[SERVER]: Is running on port 3000.");
 });
